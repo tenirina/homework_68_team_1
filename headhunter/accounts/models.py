f14569d808b620cf5f2a41d65802ddd3f88b5462
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-13 00:24:04 +0600

    Изменено verbose_name в поле workers

--- headhunter/accounts/models.py
+++ headhunter/accounts/models.py
@@ -29,7 +29,7 @@ class Account(AbstractUser):
         null=False, 
         blank=False, 
         choices=RoleChoices.choices, 
-        verbose_name='Категория продукта'
+        verbose_name='Выбирете свою роль'
     )
 
     USERNAME_FIELD = 'email'
