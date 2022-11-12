25876a93e786c60a9286c679b5820d95f80962e5
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-13 00:25:05 +0600

    Добавлены представления для регистрации и авторизации

--- headhunter/accounts/views.py
+++ headhunter/accounts/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import TemplateView, CreateView, DetailView, UpdateView
+from django.contrib.auth.models import User
 
-# Create your views here.
+from accounts.forms import LoginForm, CustomUserCreationForm
+
+
+
+class RegisterView(CreateView):
+    template_name = 'register.html'
+    form_class = CustomUserCreationForm
+    success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        next = request.GET.get('next')
+        form_data = {} if not next else {'next': next}
+        form = self.form_class(form_data)
+        print(form)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        context = {}
+        context['form'] = form
+        return self.render_to_response(context)
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    form = LoginForm
+
+    def get(self, request, *args, **kwargs):
+        next = request.GET.get('next')
+        form_data = {} if not next else {'next': next}
+        form = self.form(form_data)
+        print(form)
+        context = {'form': form}
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form(request.POST)
+        if not form.is_valid():
+            return redirect('login')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        next = form.cleaned_data.get('next')
+        user = authenticate(request, email=email, password=password)
+        if not user:
+            return redirect('login')
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('index')
