c37acbbf4883e194f2a4c61fdfd646028e6cdb52
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-12 17:42:12 +0600

    Добавлена модель Account

--- headhunter/accounts/models.py
+++ headhunter/accounts/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from accounts.managers import UserManager
+from django.db.models import TextChoices
 
-# Create your models here.
+
+
+class RoleChoices(TextChoices):
+    WORKER = 'worker', 'Работадатель'
+    EMPLOYER = 'employer', 'Соискатель'
+
+
+class Account(AbstractUser):
+    email = models.EmailField(verbose_name='Электронная почта', unique=True, blank=False, null=False)
+
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to='avatars',
+        verbose_name='Аватар'
+    )
+    birthday = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name='Дата рождения'
+    )
+    worker = models.CharField(
+        max_length=30, 
+        null=False, 
+        blank=False, 
+        choices=RoleChoices.choices, 
+        verbose_name='Категория продукта'
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Профиль'
+        verbose_name_plural = 'Профили'
