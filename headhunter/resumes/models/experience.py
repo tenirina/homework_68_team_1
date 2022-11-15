f0e0d166c764001e372ea9d4bf5efb00a64f3837
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-16 02:59:16 +0600

    #3 ADD: Создана модель для опыта работы

---
+++ headhunter/resumes/models/experience.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+
+class Experience(models.Model):
+
+    company_name = models.CharField(
+        max_length=150,
+        blank=False,
+        null=False,
+        verbose_name='Наименование компании'
+    )
+    position = models.CharField(
+        max_length=150,
+        blank=False,
+        null=False,
+        verbose_name='Должность'
+    )
+    start_work = models.DateField(
+        verbose_name='Дата начала работы',
+        null=False,
+        blank=False
+    )
+    end_work = models.DateField(
+        verbose_name='Дата окончания работы',
+        null=False,
+        blank=False
+    )
+    resume = models.ForeignKey(
+        to='resumes.Resume',
+        related_name='resume_exp',
+        null=False,
+        blank=False,
+        verbose_name='Резюме',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.company_name} - {self.position}'
