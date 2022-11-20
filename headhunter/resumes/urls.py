903e71c03d92b236bfa85dceef89ef911830f749
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-21 01:36:35 +0600

    #3 ADD: Прописаны пути для детальной страницы резюме, добавления опыта работы, добавления место учебы

--- headhunter/resumes/urls.py
+++ headhunter/resumes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from resumes.views import ResumeCreateView
+from resumes.views import ResumeCreateView, ResumeEditView, ResumeDetailView, EducationCreateView, ExperienceCreateView
 
 urlpatterns = [
     path('create/', ResumeCreateView.as_view(), name='resume_create'),
-    path('<int:pk>/edit/', ResumeCreateView.as_view(), name='resume_edit')
+    path('<int:pk>/edit/', ResumeEditView.as_view(), name='resume_edit'),
+    path('<int:pk>/detail/', ResumeDetailView.as_view(), name='resume_detail'),
+    path('<int:pk>/create/experience', ExperienceCreateView.as_view(), name='resume_experience_create'),
+    path('<int:pk>/create/education', EducationCreateView.as_view(), name='resume_education_create')
 ]
