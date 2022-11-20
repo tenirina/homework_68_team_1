0522df75dd77a6d6013e982a1e40f37053ef549c
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-21 01:38:34 +0600

    #3 ADD: Внесены изменения для редактирования резюме, добавлено представления для детального просмотра резюме, для добавления опыта и образования

--- headhunter/resumes/views.py
+++ headhunter/resumes/views.py
@@ -4,6 +4,7 @@ from django.views.generic import UpdateView, CreateView, DeleteView, DetailView
 from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
 
 
 
@@ -34,11 +35,72 @@ class ResumeEditView(UpdateView):
         form = self.form_class(request.POST, request.FILES)
         if form.is_valid():
             resume = get_object_or_404(Resume, pk=kwargs.get('pk'))
+            resume.resume_title = request.POST['resume_title']
+            resume.salary = request.POST['salary']
+            resume.about_yourself = request.POST['about_yourself']
+            resume.email = request.POST['email']
+            resume.phone = request.POST['phone']
+            resume.telegram = request.POST['telegram']
+            resume.linkedin = request.POST['linkedin']
+            resume.facebook = request.POST['facebook']
             resume.save()
-            return redirect('profile', pk=self.request.user.pk)
+            return redirect('resume_detail', pk=resume.pk)
         context = {}
         context['form'] = form
-        return render('profile', pk=self.request.user.pk)
+        return render('resume_detail', pk=self.resume.pk)
 
     def get_success_url(self, request):
-        return reverse('profile', kwargs={'pk': self.object.author})
+        resume = request.POST.get('id')
+        print(resume)
+        return reverse('resume_detail', pk=resume.pk)
+
+
+class ResumeDetailView(DetailView):
+    template_name = 'resume_detail.html'
+    model = Resume
+    context_object_name = 'resume'
+
+    def get_context_data(self,  **kwargs):
+        context = super().get_context_data(**kwargs)
+        experiences = Experience.objects.filter(resume_id=self.object.pk)
+        education = Education.objects.filter(resume_id=self.object.pk)
+        context['experiences'] = experiences
+        context['education'] = education
+        return context
+
+class ExperienceCreateView(CreateView):
+    form_class = ExperienceForm
+
+    def post(self, request, *args, **kwargs):
+        resume = Resume.objects.get(pk=kwargs['pk'])
+        company_name = request.POST.get('company_name')
+        position = request.POST.get('position')
+        start_work = request.POST.get('start_work')
+        end_work = request.POST.get('end_work')
+        Experience.objects.create(
+            company_name=company_name,
+            position=position,
+            start_work=start_work,
+            end_work=end_work,
+            resume=resume,
+        )
+        return HttpResponse(status=201)
+
+
+class EducationCreateView(CreateView):
+    from_class = EducationForm
+
+    def post(self, request, *args, **kwargs):
+        resume = Resume.objects.get(pk=kwargs['pk'])
+        place_of_education = request.POST.get('place_of_education')
+        specialization = request.POST.get('specialization')
+        start_education = request.POST.get('start_education')
+        end_education = request.POST.get('end_education')
+        Education.objects.create(
+            place_of_education=place_of_education,
+            specialization=specialization,
+            start_education=start_education,
+            end_education=end_education,
+            resume=resume,
+        )
+        return HttpResponse(status=201)
