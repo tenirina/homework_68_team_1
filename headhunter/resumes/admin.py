84cb0e88e0461e6014510e20a7745c5bfd22d7e3
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-21 01:35:34 +0600

    #3 ADD: Добавлены поля Experience и Education в админ-панель

--- headhunter/resumes/admin.py
+++ headhunter/resumes/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from resumes.models import Resume
+from resumes.models import Resume, Experience, Education
 
+class ExperienceInline(admin.StackedInline):
+    model = Experience
+    fields = ('company_name', 'position', 'start_work', 'end_work')
+
+class EducationInline(admin.StackedInline):
+    model = Education
+    fields = ('place_of_education', 'specialization', 'start_education', 'end_education')
 
 
 class ResumeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'resume_title', 'salary', 'profession', 'phone', 'email', 'about_yourself', 'telegram', 'linkedin', 'facebook')
-    search_fields = ('resume_title', 'salary', 'profession', 'phone', 'email', 'about_yourself', 'telegram', 'linkedin', 'facebook')
-    fields = ('resume_title', 'salary', 'profession', 'phone', 'email', 'about_yourself', 'telegram', 'linkedin', 'facebook')
+    inlines = (ExperienceInline, EducationInline, )
+    # list_display = ('id', 'resume_title', 'salary', 'profession', 'phone', 'email', 'about_yourself', 'telegram', 'linkedin', 'facebook')
+    # search_fields = ('resume_title', 'salary', 'profession', 'phone', 'email', 'about_yourself', 'telegram', 'linkedin', 'facebook')
+    # fields = ('resume_title', 'salary', 'profession', 'phone', 'email', 'about_yourself', 'telegram', 'linkedin', 'facebook')
 
 
 admin.site.register(Resume, ResumeAdmin)
