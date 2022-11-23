06252333e3fcc8d4bd6ac762aa5d4f0dbf99fa4f
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-24 00:44:06 +0600

    #5 ADD: Вакансии зарегестрированны в админ панеле

---
+++ headhunter/vacancies/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from vacancies.models.vacancies import Vacancy
+
+
+
+class VacansiesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'title', 'salary', 'description', 'profession', 'experience_from', 'experience_before', 'created_at', 'updated_at')
+    search_fields = ('author', 'title', 'salary', 'description', 'profession', 'experience_from', 'experience_before')
+    fields = ('author', 'title', 'salary', 'description', 'profession', 'experience_from', 'experience_before')
+
+
+admin.site.register(Vacancy, VacansiesAdmin)
