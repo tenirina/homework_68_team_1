c0def0923006a6f78de3a2aa520ba95e53cb88e4
Yermurat Temirzhanov <scaleneend@gmail.com>
2022-11-24 00:44:35 +0600

    #5 Испралены синтакситечкая ошибка

--- headhunter/vacancies/admin.py
+++ headhunter/vacancies/admin.py
@@ -3,10 +3,10 @@ from vacancies.models.vacancies import Vacancy
 
 
 
-class VacansiesAdmin(admin.ModelAdmin):
+class VacanсiesAdmin(admin.ModelAdmin):
     list_display = ('id', 'author', 'title', 'salary', 'description', 'profession', 'experience_from', 'experience_before', 'created_at', 'updated_at')
     search_fields = ('author', 'title', 'salary', 'description', 'profession', 'experience_from', 'experience_before')
     fields = ('author', 'title', 'salary', 'description', 'profession', 'experience_from', 'experience_before')
 
 
-admin.site.register(Vacancy, VacansiesAdmin)
+admin.site.register(Vacancy, VacanсiesAdmin)
